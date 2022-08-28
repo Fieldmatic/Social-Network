e030b0594c3bb36723b3f497dd14a5f8bf752071
ivana.ste <stevanovic.sw69.2019@uns.ac.rs>
2022-08-28 15:29:22 +0200

    registration page finished

--- static/app/login.js
+++ static/app/login.js
@@ -1,24 +1,52 @@
 var login = new Vue({
     el: '#login',
     data: {
-        user : {}
+        user : {},
+        registered : true
     },
     methods: {
         login : function() {
-            console.log(this.user.username)
-            console.log(this.user.password)
-
-            axios.post("/authentication/login", this.user)
-                .then(response => {
-                    (toast("Successfully logged in!"))
-                    console.log(response.data);
-                    window.location.href = "/homePage.html";
+            console.log(this.user)
+            console.log(this.registered)
+            if (this.registered) {
+                axios.post("/authentication/login", this.user)
+                    .then(response => {
+                            (toast("Successfully logged in!"))
+                            console.log(response.data);
+                            window.location.href = "/homePage.html";
+                        }
+                    ).catch(function error(err) {
+                    toast(err.response.data);
+                });
+            } else {
+                let userRegister = {
+                    username : this.user.username,
+                    password : this.user.password,
+                    email : this.user.email,
+                    name : this.user.name,
+                    surname : this.user.surname,
+                    birthDate : this.user.birthDate.replace(/-/g, '.'),
+                    gender : this.user.gender,
+                    privateAccount : true
                 }
-            ).catch(function error(err) {
-                toast(err.response.data);
-            });
-
-
+                axios.post("/authentication/register", JSON.stringify(userRegister))
+                    .then(response => {
+                            (toast("Successfully registered!"))
+                            console.log(response.data);
+                            window.location.href = "/homePage.html";
+                        }
+                    ).catch(function error(err) {
+                    toast(err.response.data);
+                });
+            }
+        },
+        changeSignUpScreen : function () {
+            this.registered = !this.registered
+        },
+        onChange(event) {
+            var optionText = event.target.value;
+            console.log(optionText);
+            this.user.gender = optionText
         }
     }
 })
